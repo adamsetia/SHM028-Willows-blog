8448375140c06e608801439713a83151a1128471
Adam Tengku <adam@bang.com.au>
2015-04-15 05:11:09 +1000

    next pref btn
    Signed-off-by: Adam Tengku <adam@bang.com.au>

--- inc/nav.php
+++ inc/nav.php
@@ -1,4 +1,10 @@
-<div class="navigation">
-	<div class="next-posts"><?php next_posts_link('&laquo; Older Entries') ?></div>
-	<div class="prev-posts"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+<?php if(get_next_posts_link() || get_previous_posts_link()): ?>
+<div class="nav-pager clearfix">
+	<?php if(get_next_posts_link()): ?>
+	<div class="next-posts"><?php next_posts_link('Take me back') ?></div>
+    <?php endif; ?>
+    <?php if(get_previous_posts_link()): ?>
+	<div class="prev-posts"><?php previous_posts_link('Show me more') ?></div>
+    <?php endif; ?>
 </div>
+<?php endif; ?>
